579564fbd7c6f74e7ae331896d111709d9b3bd6e
ydvarchana10-ux <ydvarchana10@gmail.com>
2026-03-30 20:45:13 -0400

    Create TabularDataset and split data into train/test sets
    Implemented TabularDataset creation and data splitting.

--- train.py
+++ train.py
@@ -54,14 +54,20 @@ def main():
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
+    # ds = ### YOUR CODE HERE ###
+    ds = TabularDatasetFactory.from_delimited_files(
+    path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    )
+  
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
+    )
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
